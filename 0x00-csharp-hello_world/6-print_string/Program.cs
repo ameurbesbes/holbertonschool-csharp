cd8cad069880ef6bb8a6ef426ad9167d105366bc
ameurbesbes <ameurbesbes@outlook.com>
2022-01-04 14:57:37 +0100

    .\6-print_string\

---
+++ 0x00-csharp-hello_world/6-print_string/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+	    
+		Console.WriteLine($"{str}{str}{str}");
+
+		for (int i = 0; i < 9; i++)
+		{
+			Console.Write(str[i]);
+		}
+    }
+}
