60dbe5c864cce9faa745c5f60b29b77ccf2c646e
ameurbesbes <ameurbesbes@outlook.com>
2022-01-06 14:55:47 +0100

    first push

---
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _0_positive_or_negative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
