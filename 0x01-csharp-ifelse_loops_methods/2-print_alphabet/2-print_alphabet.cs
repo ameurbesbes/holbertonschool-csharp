e67134766352f434537627915e44ae819f09f022
ameurbesbes <ameurbesbes@outlook.com>
2022-01-10 17:21:58 +0100

    .\2-print_alphabet.cs

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char alphabet = 'a'; alphabet <= 'z'; alphabet++)
+            {
+                Console.Write(alphabet + "");
+            }
+
+        }
+    }
+}
