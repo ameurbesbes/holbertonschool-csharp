f95413701ba6603f5fea692f72dc1d12cf3b0962
ameurbesbes <ameurbesbes@outlook.com>
2022-01-13 18:03:32 +0100

    .\9-add.cs

---
+++ 0x01-csharp-ifelse_loops_methods/9-add/9-add.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    class Number
+    {
+        public static int Add(int a, int b)
+        {
+            int total;
+            total = a + b;
+            return total;
+        }
+    }
