30baf7067cbc9561b2f9b5fa879ce2f6988dc589
ameurbesbes <ameurbesbes@outlook.com>
2022-01-16 19:37:18 +0100

    .\11-main.cs

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        var output = new StringBuilder();
+        for (int i = 0; i < length; i++)
+        {
+            output.Clear();
+            output.Insert(0, " ", i);
+            Console.Write($"{output}\\\n");
+        }
+        Console.Write("\n");
+    }
+}
