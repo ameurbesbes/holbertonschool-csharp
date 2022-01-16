980758e67243682081a56012b7587860383abfeb
ameurbesbes <ameurbesbes@outlook.com>
2022-01-16 19:15:44 +0100

    .\10-main.cs

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+
+
+class Line
+{
+   public static void PrintLine(int length)
+   {
+            var output = new StringBuilder();
+            for (int i = 0; i < length; i++){
+                output.Append("_");
+            }
+            Console.Write($"{output} \n");
+   }
+}
