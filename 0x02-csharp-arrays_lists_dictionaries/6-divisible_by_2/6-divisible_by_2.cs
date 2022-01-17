4d267fed8b23a938bce19e49c9e542f3290dd4b7
ameurbesbes <ameurbesbes@outlook.com>
2022-01-17 17:50:21 +0100

    .\6-divisible_by_2\

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+       var boollist = new List<bool>();
+        foreach (var item in myList)
+        {
+            boollist.Add(item % 2 == 0);
+        }
+        return boollist;
+    }
+}
