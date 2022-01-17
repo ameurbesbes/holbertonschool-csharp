3e5a8084538d38e21a00da8d9e78ee673ed04496
ameurbesbes <ameurbesbes@outlook.com>
2022-01-17 17:20:22 +0100

    .\4-print_list\

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if(size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List<int> list = new List<int>();
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < size; i++)
+        {
+            list.Add(i);
+            output.Append(i);
+            if (i != size -1)
+                output.Append(" ");
+        }
+        Console.WriteLine(output);
+        return list;
+    }
+}
