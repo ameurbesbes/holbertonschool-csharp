5f4462b7bb154ef3429efc65f4ae050a11d7dc8e
ameurbesbes <ameurbesbes@outlook.com>
2022-01-17 18:30:14 +0100

    .\12-print_sorted_dictionary\

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var sorteddickt =  myDict.OrderBy(x => x.Key);
+        foreach (KeyValuePair<string, string> pair in sorteddickt)
+        {
+            Console.WriteLine($"{pair.Key}: {pair.Value}");
+        }
+
+    }
+}  
