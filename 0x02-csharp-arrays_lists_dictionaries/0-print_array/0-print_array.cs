0b44521f0795dae6cc5d699598d8b65c2cceaec8
ameurbesbes <ameurbesbes@outlook.com>
2022-01-17 15:21:09 +0100

    .\0-print_array.cs

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text;
+
+class Array
+{
+	public static int[] CreatePrint(int size)
+    {
+
+        if (size == 0)
+            Console.WriteLine("");
+        else if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        
+        int[] array = new int[size];
+        var output = new StringBuilder();
+        for (int i = 0; i < size; i++)
+        {
+            output.Append(i);
+            if (i < size - 1)
+                output.Append(" ");
+        }
+        Console.WriteLine(output);
+        return array;
+    }
+}
