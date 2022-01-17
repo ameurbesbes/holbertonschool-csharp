fe9af68d133c2834c550f5de243d7210e980ab78
ameurbesbes <ameurbesbes@outlook.com>
2022-01-17 18:47:41 +0100

    .\13-best_score\

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int i = int.MinValue;
+        string keyofi = "None";
+        foreach(KeyValuePair<string,int>pair in myList)
+        {
+            if (pair.Value > i)
+            {
+                i = pair.Value;
+                keyofi = pair.Key;
+            }
+        }
+        return keyofi;
+    }
+}
