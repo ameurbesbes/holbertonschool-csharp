8c096e9e50176c5f79a639d0f78c24973d00eff5
ameurbesbes <ameurbesbes@outlook.com>
2022-01-17 17:04:02 +0100

    .\3-print_array_reverse\

--- 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -5,21 +5,17 @@ class Array
     public static void Reverse(int[] array)
     {
         
-        if (array != null && array.Length > 0)
-        {
-            char sep = ' ';
-            for (var i = array.Length - 1; i >= 0; i--)
-            {
-                Console.Write($"{array[i]}{sep}");
-                if (i == 0)
-                    sep = '\n';
-            }  
-        }
-        else
+        if (array == null || array.Length == 0)
         {
             Console.WriteLine();
-            
-        }
-
+            return;
+        }     
+        char sep = ' ';
+        for (var i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write($"{array[i]}{sep}");
+            if (i == 0)
+                sep = '\n';
+        }  
     }
 }
