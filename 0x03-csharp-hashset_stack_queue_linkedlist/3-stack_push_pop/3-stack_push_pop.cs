07259911bda5777d8886a3e6810d64ec78b92aa4
ameurbesbes <ameurbesbes@outlook.com>
2022-02-09 14:55:28 +0100

    .\3-stack_push_pop\

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        var numberofelem = aStack.Count();
+        Console.WriteLine($"Number of items: {numberofelem}");
+        if (numberofelem == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        }
+        bool containsearch = aStack.Contains(search);
+        Console.WriteLine($"Stack contains \"{search}\": {containsearch}");
+        while (aStack.Contains(search))
+        {
+            aStack.Pop();
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
