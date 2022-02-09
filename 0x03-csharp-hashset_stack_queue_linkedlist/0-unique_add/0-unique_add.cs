847d4a29dc0747e2f064a230024e49ce8604cd84
ameurbesbes <ameurbesbes@outlook.com>
2022-02-09 12:03:14 +0100

    .\0-unique_add.cs

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        var newvar = myList.Distinct().ToList();
+        int sum = 0;
+        foreach (var item in newvar)
+        {
+            sum = sum + item;
+        }
+        return sum;
+    }
+}
