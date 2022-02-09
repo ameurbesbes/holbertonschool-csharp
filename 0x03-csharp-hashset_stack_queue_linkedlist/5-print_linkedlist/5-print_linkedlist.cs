1c7db61d7c4e17ada676d0cb149d6f55ba0bbd70
ameurbesbes <ameurbesbes@outlook.com>
2022-02-09 15:48:27 +0100

    .\5-print_linkedlist\

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> list = new LinkedList<int>();
+        if (size < 0)
+        {
+            return list;
+        }
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine(i);
+            list.AddFirst(i);
+        }
+        return list;
+    }
+}
