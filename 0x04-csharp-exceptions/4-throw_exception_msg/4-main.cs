5f9eb903d1b2dee118ee6e22657ef0eadc71ed3a
ameurbesbes <ameurbesbes@outlook.com>
2022-02-21 13:36:28 +0100

    throw-exc-msg

---
+++ 0x04-csharp-exceptions/4-throw_exception_msg/4-main.cs
@@ -0,0 +1,17 @@
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Except.ThrowMsg("C is fun");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
