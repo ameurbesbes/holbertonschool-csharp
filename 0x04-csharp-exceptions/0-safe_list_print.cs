8f0e8fac70d915c8172392654e218a009e827c37
ameurbesbes <ameurbesbes@outlook.com>
2022-02-21 11:42:52 +0100

    .\0x04-csharp-exceptions\

---
+++ 0x04-csharp-exceptions/0-safe_list_print.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _0x04_csharp_exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
