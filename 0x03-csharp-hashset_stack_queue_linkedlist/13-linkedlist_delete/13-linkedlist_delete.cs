1b4b7d85e1c77771956889e637a8eff43efd2906
ameurbesbes <ameurbesbes@outlook.com>
2022-02-22 10:53:36 +0100

    linkedlist_delete

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int i = 0;
+        foreach (int x in myLList)
+        {
+            if (i == index)
+            {
+                myLList.Remove(x);
+                return;
+            }
+            i++;
+        }
+    }
+}
