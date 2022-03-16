8be204c8a76d5396838e9f23c263d502fdf30fd3
ameurbesbes <ameurbesbes@outlook.com>
2022-03-16 21:26:18 +0100

    .\1-enemy\

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// empty zombie class
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>Field that defines the health of zombie.</summary> 
+        public int health;
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
