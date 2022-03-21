00da7ae1edbc59945632747f7ace0e22662f2e14
ameurbesbes <ameurbesbes@outlook.com>
2022-03-21 14:50:50 +0100

    last push

---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyMath
+{
+    public class Operations
+    {
+        /// <summary>
+        /// Add: Method
+        /// </summary>
+        /// <param name="a">int </param>
+        /// <param name="b">int</param>
+        /// <returns>int</returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
