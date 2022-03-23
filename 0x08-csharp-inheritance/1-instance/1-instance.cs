adb9f96783528fdaac868b97b66d87bbe4bad0e2
ameurbesbes <ameurbesbes@outlook.com>
2022-03-23 22:33:21 +0100

    inheritance

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>Represents the class Obj.</summary>
+class Obj
+{
+    /// <summary>
+    /// check if obj insatnce of array or sub
+    /// </summary>
+    /// <param name="obj">obj to check</param>
+    /// <returns> True or  False </returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj.GetType() == typeof(Array) || obj is Array;
+    }
+
+}
---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+class Obj
+{
+    /// <summary>
+    /// Prints the names of the available properties and methods of an object.
+    /// </summary>
+    /// <param name="myObj">Object to use</param>
+    public static void Print(object myObj)
+    {
+        PropertyInfo[] p = myObj.GetType().GetProperties();
+        MethodInfo [] m = myObj.GetType().GetMethods();
+
+        Console.WriteLine($"{myObj.GetType().Name} Properties:");
+        foreach(var property in p)
+        {
+            Console.WriteLine(property.Name);
+        }
+
+        Console.WriteLine($"{myObj.GetType().Name} Methods:");
+        foreach (var method in m) 
+        {
+            Console.WriteLine(method.Name); 
+        }
+    }
+}
