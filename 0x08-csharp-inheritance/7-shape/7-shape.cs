37d869e678900b7936010f99fa67670e6d7c8c80
ameurbesbes <ameurbesbes@outlook.com>
2022-03-24 19:11:38 +0100

    .\7-shape\

---
+++ 0x08-csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,61 @@
+﻿using System;
+/// <summary>
+/// Base class for shapes. 
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Not Implemented yet
+    /// </summary>
+    /// <returns>An integer</returns>
+    public virtual int Area() => throw new NotImplementedException("Area() is not implemented");
+}
+/// <summary>
+/// Rectangle: Class that inherits from Shape
+/// </summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+    /// <summary>
+    /// width setter and getter
+    /// </summary>
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            width = value;
+        }
+    }
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            height = value;
+        }
+    }
+    /// <summary>
+    /// returns the area of the rectangle
+    /// </summary>
+    /// <returns></returns>
+    public new int Area()
+    {
+        return (width * height);
+    }
+    public override string ToString()
+    {
+        return $"[Rectangle] {width} / {height}";
+    }
+}
