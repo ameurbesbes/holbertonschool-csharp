ae758475db7e81d1a231a3f99cda67818f457e84
ameurbesbes <ameurbesbes@outlook.com>
2022-04-03 01:54:06 +0200

    .\3-magnitude\

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,16 +1,17 @@
 ﻿using System;
+
 static class VectorMath
 {
-        public static double Magnitude(double[] vector)
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length == 2)
         {
-            if (vector.Length == 2)
-                {
-                return Math.Round(Math.Sqrt(Math.Pow(vector[0],2) + Math.Pow(vector[1],2)));
-                }
-            if (vector.Length == 3)
-                {
-            return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)));
-                }
-        return -1; 
+            return Math.Round(Math.Sqrt(Math.Pow(vector[0],2) + Math.Pow(vector[1],2)));
         }
+        if (vector.Length == 3)
+        {
+            return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)));
+        }
+        return -1; 
+    }
 }
