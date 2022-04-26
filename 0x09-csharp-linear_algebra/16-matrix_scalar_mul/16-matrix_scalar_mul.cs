e51d23d007bd205a919262b79d682b5bb7ab3fad
ameurbesbes <ameurbesbes@outlook.com>
2022-04-26 16:27:25 +0200

    update

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/// <summary>
+/// MatrixMath class
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Multiply matrix by scalar
+    /// </summary>
+    /// <param name="matrix"> Matrix 2D or 3D</param>
+    /// <param name="scalar">Scalar</param>
+    /// <returns>the product of matrice and scalar or -1 if not possible</returns>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        if (matrix.Length <= 1 ||
+           matrix.GetLength(0) != matrix.GetLength(1))
+        {
+            return (new Double[,] { { -1 } });
+        }
+        double[,] res = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                res[i, j] = matrix[i, j] * scalar;
+            }
+        }
+        return res;
+
+    }
+}
---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,27 @@
+﻿using System;
+/// <summary>
+/// VectorMath class
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// multiplies a vector and a scalar and returns the resulting vector.
+    /// </summary>
+    /// <param name="vector">vector</param>
+    /// <param name="scalar">vector</param>
+    /// <returns>resulting vector</returns>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length != 2 && vector.Length != 3)
+        {
+            return new double[1] { -1 };
+        }
+        var res = new double[vector.Length];
+        for(var i=0; i < vector.Length;i++)
+        {
+            res[i] = vector[i] * scalar;
+        }
+        return res;
+
+    }
+}
