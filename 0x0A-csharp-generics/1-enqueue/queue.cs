ce005fa82553eb773b8394cdd661701a469dc7b3
ameurbesbes <ameurbesbes@outlook.com>
2022-05-04 00:13:51 +0200

    last-push

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+/// <summary>
+/// Custom generic class Queue
+/// </summary>
+/// <typeparam name="T">Could be any type</typeparam>
+
+class Queue<T>
+{
+
+
+    public Node head { get; set; }
+    public Node tail { get; set; }
+    private int count;
+    /// <summary>
+    /// Insert node in the Queue 
+    /// </summary>
+    /// <param name="value"> the value of the  node </param>
+    public void Enqueue(T value)
+    {
+        Node n = new Node(value);
+        if (count == 0)
+        {
+            head = n;
+            tail = n;
+            head.next = null;
+        }
+        else
+        {
+            tail.next = n;
+        }
+        this.count += 1;
+    }
+    /// <summary>
+    /// Checks the type of this generic class.
+    /// </summary>
+    /// <returns>Returns the type of the generic class.</returns>
+    public String CheckType()
+    {
+        return typeof(T).ToString();
+    }
+    /// <summary>
+    /// Get the number of nodes in the queue 
+    /// </summary>
+    /// <returns>the number of node in queue</returns>
+    public int Count()
+    {
+        return this.count;
+    }/// <summary>
+     /// The Queue of Node 
+     /// </summary>
+    public class Node
+    {
+        private T value { get; set; }
+        public Node next { get; set; }
+        public Node(T value)
+        {
+            this.value = value;
+        }
+
+    }
+}
---
+++ 0x0A-csharp-generics/5-concatenate/queue.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Text;
+
+/// <summary>
+/// Custom generic class Queue
+/// </summary>
+/// <typeparam name="T">Could be any type</typeparam>
+
+class Queue<T>
+{
+
+
+    public Node head { get; set; }
+    public Node tail { get; set; }
+    private int count;
+    /// <summary>
+    /// Insert node in the Queue 
+    /// </summary>
+    /// <param name="value"> the value of the  node </param>
+    public void Enqueue(T value)
+    {
+        Node n = new Node(value);
+        if (count == 0)
+        {
+            head = n;
+            tail = n;
+        }
+        else
+        {
+            tail.next = n;
+            tail = n;
+        }
+        this.count += 1;
+    }
+    /// <summary>
+    /// Remove a node from the Queue
+    /// </summary>
+    /// <returns>the default value of the parametre type or the value of node removed</returns>
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return (default(T));
+        }
+        else
+        {
+            T tmp = head.value;
+            head = head.next;
+            count -= 1;
+            return (tmp);
+        }
+    }
+    public T Peek()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return (default(T));
+        }
+        else
+        {
+            return (head.value);
+        }
+    }
+    public void Print()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        while (head != null)
+        {
+            Console.WriteLine(head.value);
+            head = head.next;
+        }
+    }
+    /// <summary>
+    /// Concat all values in the Queue
+    /// </summary>
+    /// <returns>String or null</returns>
+    public String Concatenate()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return null;
+        }
+        if (typeof(T) == typeof(String) || typeof(T) == typeof(Char))
+        {
+            var res = new StringBuilder("");
+            while (head != null)
+            {
+                res.Append(head.value);
+                if (typeof(T) == typeof(String))
+                {
+                    res.Append(" ");
+                }
+                head = head.next;
+
+            }
+            return (res.ToString());
+
+
+        }
+        Console.WriteLine("Concatenate() is for a queue of Strings or Chars only.");
+        return (null);
+
+    }
+    /// <summary>
+    /// Checks the type of this generic class.
+    /// </summary>
+    /// <returns>Returns the type of the generic class.</returns>
+    public String CheckType()
+    {
+        return typeof(T).ToString();
+    }
+    /// <summary>
+    /// Get the number of nodes in the queue 
+    /// </summary>
+    /// <returns>the number of node in queue</returns>
+    public int Count()
+    {
+        return this.count;
+    }/// <summary>
+     /// The Queue of Node 
+     /// </summary>
+    public class Node
+    {
+        public T value { get; set; }
+        public Node next { get; set; }
+        public Node(T value)
+        {
+            this.value = value;
+        }
+
+    }
+}
