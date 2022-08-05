066025c6587a52cf07b3120bb9e1b526a36b025f
ameurbesbes <ameurbesbes@outlook.com>
2022-08-05 14:06:37 +0200

    fix code

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public abstract class Base
+{
+    /// <summary>
+    /// Name of the object.
+    /// </summary>
+    public string name { get; set; }
+    /// <summary>
+    /// Overide ToString Method
+    /// </summary>
+    public override string ToString() => $"{this.name} is a {this.GetType()}";
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+/// <summary>
+/// Test class for interfaces
+/// </summary>
+public class TestObject: Base, IInteractive, IBreakable, ICollectable
+{
+    public void Interact(){}
+    public int durability { get; set; }
+    public void Break(){}
+    public void Collect(){}
+    public bool isCollected { get; set;}
+}
