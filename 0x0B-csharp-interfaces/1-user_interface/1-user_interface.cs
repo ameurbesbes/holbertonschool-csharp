0cf58e1b33cb94bf6ee213ba6ebce07cb144cfe9
ameurbesbes <ameurbesbes@outlook.com>
2022-08-17 10:32:15 +0200

    last push

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-
+/// <summary>
+/// obstract class
+/// </summary>
 public abstract class Base
 {
     /// <summary>
@@ -11,21 +13,42 @@ public abstract class Base
     /// </summary>
     public override string ToString() => $"{this.name} is a {this.GetType()}";
 }
-
+/// <summary>
+/// interface IIdentifiable
+/// </summary>
 interface IInteractive
 {
+    /// <summary>
+    /// interact()
+    /// </summary>
     void Interact();
 }
-
+/// <summary>
+/// interface IBreakable
+/// </summary>
 interface IBreakable
 {
+    /// <summary>
+    /// int
+    /// </summary>
     int durability { get; set; }
+    /// <summary>
+    /// method Break()
+    /// </summary>
     void Break();
 }
-
+/// <summary>
+/// interface ICollectable
+/// </summary>
 interface ICollectable
 {
+    /// <summary>
+    /// bool isCollected
+    /// </summary>
     bool isCollected { get; set; }
+     /// <summary>
+    /// collect()
+    /// </summary>
     void Collect();
 }
 
@@ -34,9 +57,24 @@ interface ICollectable
 /// </summary>
 public class TestObject: Base, IInteractive, IBreakable, ICollectable
 {
+    /// <summary>
+    /// Interact()
+    /// </summary>
     public void Interact(){}
+    /// <summary>
+    /// int durability
+    /// </summary>
     public int durability { get; set; }
+    /// <summary>
+    /// Break()
+    /// </summary>
     public void Break(){}
+    /// <summary>
+    /// Collect()
+    /// </summary>
     public void Collect(){}
+    /// <summary>
+    /// bool isCollected
+    /// </summary>
     public bool isCollected { get; set;}
 }
