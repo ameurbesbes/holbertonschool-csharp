9f9efebd0d41a331f678ad2b2504659e8a0a91f8
ameurbesbes <ameurbesbes@outlook.com>
2022-08-29 16:04:04 +0200

    fix code

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -14,7 +14,7 @@ public class Player
     /// <param name="maxHp"> the max hp of the player</param>
     public Player(string name = "Player", float maxHp=100f)
     {
-        if (maxHp < 0)
+        if (maxHp <= 0)
         {
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
             this.maxHp = 100f;
