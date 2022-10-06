f273dd3d5e5ad96f07d7b6f01b40cb7afae5a7f7
ameurbesbes <ameurbesbes@outlook.com>
2022-10-06 16:24:18 +0200

    UP_date

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/JSONStorage.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace JSONStorage
+{
+    public class JSONStorage
+    {
+
+
+        public Dictionary<string, dynamic> objects = new Dictionary<string, dynamic>();
+
+        public Dictionary<string, dynamic> all()
+        {
+            return objects;
+        }
+        public void New(obj)
+        {
+            try
+            {
+                objects.Add($"{obj.GetType().Name{obj.id}", obj);
+            }
+            catch (Exeption)
+            {
+                Console.WriteLine("provide Class Instance That Inhirit from BaseClass");
+            }
+        }
+        public void Save()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string fileName = "storage/inventory_manager.json";
+            string jsonString = JsonSerializer.Serialize(objects, options);
+            File.WriteAllText(fileName, jsonString);
+        }
+        public void Load()
+        {
+            string fileName = "storage/inventory_manager.json";
+            string jsonString = File.ReadAllText(fileName);
+            objects = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(jsonString);
+        }
+    }
+
+}
